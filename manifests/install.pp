a0eb1b0320ea77c4324c4eee73851616a57aa2ad
PierrickI3 <pierrick.lozach@inin.com>
2015-03-16 17:56:21 +0100

    Removed the variables comments section

--- manifests/install.pp
+++ manifests/install.pp
@@ -52,17 +52,6 @@
 # [defaulticpassword]
 #   Default IC user password. Defaults to 1234.
 #
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
-#
 # === Examples
 #
 #  class {'cicserver::install':
