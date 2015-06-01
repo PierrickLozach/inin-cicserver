b8889d59891ae40e3bebcc7c8160a93e7e821947
PierrickI3 <pierrick.lozach@inin.com>
2015-06-01 10:29:21 +0200

    Adding compile test

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 describe 'icsurvey' do
 
+  context 'it should compile' do
+  	it {should compile}
+  end
+  
   context 'with defaults for all parameters' do
     it { should contain_class('icsurvey') }
   end
