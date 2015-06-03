7a5e133870953c94e383ca2a5a2c2de3a325b466
PierrickI3 <pierrick.lozach@inin.com>
2015-06-03 11:38:43 +0200

    More tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -25,12 +25,39 @@ describe 'cicserver::icsurvey' do
     end
   end
 
-  context 'should create manifest dir' do
-  	it { should contain_file('C:\\\\I3\\\\IC\\\\Manifest') }
+  context 'should take care of Windows before starting' do
+    it { should contain_exec('disable-automatic-maintenance') } # Turn off Windows maintenance before running MSIs
   end
 
   context 'should create survey' do
+    it { should contain_file('C:\\\\I3\\\\IC\\\\Manifest') }
   	it { should contain_file('icsurvey') }
+  end
+
+  context 'should configure CIC' do
+    it { should contain_exec('setupassistant-run') } # Run Setup Assistant
+    it { should contain_service('cicserver-service-start') } # Start CIC service
+  end
+
+  context 'should install and configure Media Server' do
+
+    # Install GA
+    it { should contain_exec('mount-cic-iso') }
+    it { should contain_package('mediaserver')}
+    it { should contain_exec('unmount-cic-iso') }
+
+    # Install Latest patch
+    it { should contain_exec('mount-cic-latest-patch') }
+    it { should contain_file('mediaserver-latest-patch-script') }
+    it { should contain_exec('mediaserver-latest-patch-run') }
+    it { should contain_exec('unmount-cic-latest-patch') }
+
+    # Configure
+    it { should contain_file('c:/i3/ic/mediaserverlicense.i3lic') } # Create the license file
+    it { should contain_exec('ININMediaServer-Start') } # Start the service
+    it { should contain_file('mediaserver-pairing') } # Create a powershell script to pair media server with CIC
+    it { should contain_exec('mediaserver-pair-cic') } # Run the powershell script
+
   end
 
 end
